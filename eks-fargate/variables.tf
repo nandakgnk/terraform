9cfc94e081c8c26a7b7653b3809710e7877c0814
nandakgnk <41625826+nandakgnk@users.noreply.github.com>
2022-08-16 16:33:36 +0530

    Create variables.tf

---
+++ eks-fargate/variables.tf
@@ -0,0 +1,19 @@
+ variable "private_subnet_id_1" {
+  type = string
+  default = "subnet-xxxxxxxxxx"
+ }
+ 
+ variable "private_subnet_id_2" {
+  type = string
+  default = "subnet-xxxxxxxx"
+ }
+
+variable "public_subnet_id_3" {
+  type = string
+  default = "subnet-xxxxxxxx"
+ }
+ 
+variable "public_subnet_id_4" {
+  type = string
+  default = "subnet-xxxxxxxxx"
+ }
